e113cd9fe0d9fdeb18c1922ff6c6b323dbde308a
mdriad <mdleton@yahoo.com>
2019-05-16 04:17:55 -0400

    working on TestHomePage

--- alibaba/src/test/java/RegressionSuite/TestHomePage.java
+++ alibaba/src/test/java/RegressionSuite/TestHomePage.java
@@ -1,4 +1,29 @@
 package RegressionSuite;
 
-public class TestHomePage {
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import regression.HomePage;
+
+public class TestHomePage extends CommonAPI {
+    HomePage homePage;
+    String url = "https://www.alibaba.com/";
+
+    @BeforeClass
+    public void init(){
+        homePage = PageFactory.initElements(driver , HomePage.class);
+        driver.get(url);
+    }
+    @AfterMethod
+    public void recall(){
+        driver.get(url);
+    }
+    @Test
+    public void click_search(){
+
+    }
+
+
 }
